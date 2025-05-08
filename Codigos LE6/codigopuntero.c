e3d6de6fa1c62acd30c9b228b76a7e9b3450244a
agusanderson23 <aanderson@etrr.edu.ar>
2025-05-07 22:44:48 -0300

    Update codigopuntero.c

--- Codigos LE6/codigopuntero.c
+++ Codigos LE6/codigopuntero.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
-#define TEMPMAXIN 30
-#define TEMPMININ 15
-#define TEMPMAXFIN 35
-#define TEMPMINFIN 20
 
 void cambiarValor(int *puntero, int nuevoValor) {
     *puntero = nuevoValor;
 }
 
-void imprimirTemperaturas(int temperaturaMaxima, int temperaturaMinima) {
+void mostrarTemperaturas(int temperaturaMaxima, int temperaturaMinima) {
     printf("Temperatura máxima: %d°C, Temperatura mínima: %d°C\n", temperaturaMaxima, temperaturaMinima);
 }
 
-void actualizarTemperaturas(int *punteroTemperaturaMaxima, int *punteroTemperaturaMinima) {
-    cambiarValor(punteroTemperaturaMaxima, TEMPMAXFIN);  
-    cambiarValor(punteroTemperaturaMinima, TEMPMINFIN);  
+void actualizarTemperaturas(int *punteroTemperaturaMaxima, int *punteroTemperaturaMinima, int nuevaMaxima, int nuevaMinima) {
+    cambiarValor(punteroTemperaturaMaxima, nuevaMaxima);  
+    cambiarValor(punteroTemperaturaMinima, nuevaMinima);  
+}
+
+void ingresarTemperaturas(int *temperaturaMaxima, int *temperaturaMinima, int *temperaturaMaximaFinal, int *temperaturaMinimaFinal) {
+    printf("Ingrese la temperatura máxima inicial: ");
+    scanf("%d", temperaturaMaxima);
+    printf("Ingrese la temperatura mínima inicial: ");
+    scanf("%d", temperaturaMinima);
+
+    printf("Ingrese la temperatura máxima final: ");
+    scanf("%d", temperaturaMaximaFinal);
+    printf("Ingrese la temperatura mínima final: ");
+    scanf("%d", temperaturaMinimaFinal);
 }
 
 int main() {
-    int temperaturaMaxima = TEMPMAXIN;
-    int temperaturaMinima = TEMPMININ;
+    int temperaturaMaxima, temperaturaMinima;
+    int temperaturaMaximaFinal, temperaturaMinimaFinal;
+
+    ingresarTemperaturas(&temperaturaMaxima, &temperaturaMinima, &temperaturaMaximaFinal, &temperaturaMinimaFinal);
+
     printf("Antes de actualizar las temperaturas:\n");
-    imprimirTemperaturas(temperaturaMaxima, temperaturaMinima);
-    actualizarTemperaturas(&temperaturaMaxima, &temperaturaMinima);
+    mostrarTemperaturas(temperaturaMaxima, temperaturaMinima);
+
+    actualizarTemperaturas(&temperaturaMaxima, &temperaturaMinima, temperaturaMaximaFinal, temperaturaMinimaFinal);
+
     printf("Después de actualizar las temperaturas:\n");
-    imprimirTemperaturas(temperaturaMaxima, temperaturaMinima);
+    mostrarTemperaturas(temperaturaMaxima, temperaturaMinima);
 
     return 0;
 }
