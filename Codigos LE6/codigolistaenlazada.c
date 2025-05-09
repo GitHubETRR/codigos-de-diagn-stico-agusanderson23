9c6dea2af748bebf37579373bac7de793ec52037
agusanderson23 <aanderson@etrr.edu.ar>
2025-05-09 10:33:30 -0300

    Update codigolistaenlazada.c

--- Codigos LE6/codigolistaenlazada.c
+++ Codigos LE6/codigolistaenlazada.c
@@ -4,89 +4,118 @@
 #define MAXNOMBRE 20
 #define VALORERROR -1
 
-typedef struct peliculas{
+typedef struct peliculas {
     char nombre[MAXNOMBRE];
     int anio;
-    struct peliculas_t *next;
-}peliculas_t
+    struct peliculas *next;
+} peliculas_t;
 
 peliculas_t *crearPelicula();
 void mostrarPeliculas(peliculas_t *lista);
 void cantidadPeliculas(peliculas_t *lista);
-void menu(peliculas_t *lista)
+void menu(peliculas_t *lista);
+void liberarMemoria(peliculas_t *lista);
+peliculas_t *agregarPelicula(peliculas_t *lista);
 
-int main(){
-    peliculas_t *listaPeliculas = NULL;
-    menu(listaPeliculas);
+int main() {
+    peliculas_t *lista = NULL;
+    menu(lista);
     return 0;
 }
-peliculas_t* crearPelicula(){
+
+peliculas_t* crearPelicula() {
     peliculas_t *nuevaPeli = (peliculas_t *)malloc(sizeof(peliculas_t));
-    if(nuevaPeli == NULL){
-        printf("Error al asignar memoria");
+    if (nuevaPeli == NULL) {
+        printf("Error al asignar memoria\n");
         exit(VALORERROR);
     }
     printf("Ingrese el nombre de la pelicula: ");
     scanf("%s", nuevaPeli->nombre);
-    printf("\n Ingrese el año de lanzamiento: ");
+    printf("Ingrese el año de lanzamiento: ");
     scanf("%d", &nuevaPeli->anio);
     nuevaPeli->next = NULL;
     return nuevaPeli;
 }
-void mostrarPeliculas(peliculas_t *lista){
-    if(lista==NULL){
-        printf("No hay peliculas almacenadas");
-        retrn;
+
+void mostrarPeliculas(peliculas_t *lista) {
+    if (lista == NULL) {
+        printf("No hay peliculas almacenadas.\n");
+        return;
     }
     peliculas_t *actual = lista;
-    printf("---- LISTA DE PELICULAS ----");
-    while(actual!=NULL){
-        printf("Nombre : %s, año: %d\n", actual->nombre, actual->anio);
-        actual=actual->next;
+    printf("---- LISTA DE PELICULAS ----\n");
+    while (actual != NULL) {
+        printf("Nombre: %s, Año: %d\n", actual->nombre, actual->anio);
+        actual = actual->next;
     }
-    printf("\n");
 }
 
-void cantidadPeliculas(peliculas_t *lista){
+void cantidadPeliculas(peliculas_t *lista) {
     int contador = 0;
-    pleiculas_t *actual = lista;
-    while(actual!=NULL){
+    peliculas_t *actual = lista;
+    while (actual != NULL) {
         contador++;
-        actual=actual->next;
+        actual = actual->next;
     }
-    printf("La cantidad de peliculas almacenadas es: %d", contador);
+    printf("La cantidad de peliculas almacenadas es: %d\n", contador);
 }
 
-void menu(peliculas_t *lista){
-    do{
-        int opcion;
-        
-        printf("---- Almacenamiento de peliculas ----");
-        printf("1. Agregar pelicula");
-        printf("2. Mostrar peliculas");
-        printf("3. Cantidad de peliculas");
-        printf("4. Salir");
-        scanf("%d", &opcion);
-    
-        switch(opcion){
-            case 1:
-            peliculas_t *nuevaPeli = crearPelicula();
-            break
-        
-            case 2:
-            mostrarPelicula();
-            break
-        
-            case 3:
-            cantidadPeliculas();
-            break
-        
-            case 4:
-            printf("Saliendo del programa...\n");
-            break
-        
-            default:
-            printf("Opcion no valida");
+void liberarMemoria(peliculas_t *lista) {
+    peliculas_t *aux;
+    while (lista != NULL) {
+        aux = lista;
+        lista = lista->next;
+        free(aux);
+    }
+    printf("Memoria liberada correctamente.\n");
+}
+
+peliculas_t *agregarPelicula(peliculas_t *lista) {
+    peliculas_t *nuevaPeli = crearPelicula();
+    if (lista == NULL) {
+        return nuevaPeli;
+    } else {
+        peliculas_t *aux = lista;
+        while (aux->next != NULL) {
+            aux = aux->next;
         }
-    }while(opcion != 4);    
+        aux->next = nuevaPeli;
+        return lista;
+    }
+}
+
+void menu(peliculas_t *lista) {
+    int opcion;
+    do {
+        printf("\n---- Almacenamiento de peliculas ----\n");
+        printf("1. Agregar pelicula\n");
+        printf("2. Mostrar peliculas\n");
+        printf("3. Cantidad de peliculas\n");
+        printf("4. Eliminar lista\n");
+        printf("5. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                lista = agregarPelicula(lista);
+                break;
+            case 2:
+                mostrarPeliculas(lista);
+                break;
+            case 3:
+                cantidadPeliculas(lista);
+                break;
+            case 4:
+                liberarMemoria(lista);
+                lista = NULL;
+                break;
+            case 5:
+                liberarMemoria(lista);
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida\n");
+        }
+    } while (opcion != 5);
 }
